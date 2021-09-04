e0d8dcff86bfc0cdd5f5d9cd0e87d7b9e14e4131
徐聪 <753428092@qq.com>
2021-09-04 23:27:22 +0800

    Vue源码

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,15 @@ cd docs/.vuepress/dist
 # echo 'www.yourwebsite.com' > CNAME
 git init
 git add .
-git commit -m 'vue源码问题'
+git commit -m 'vue源码'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f https://github.com/shamworld/shamworld.github.io.git master
+git push -f https://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@github.com/shamworld/shamworld.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
 
 cd -
 git add .
-git commit -m 'vue源码问题'
+git commit -m 'vue源码'
 git push
