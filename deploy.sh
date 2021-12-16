541b4ec9daa1caa32a57345a17d1ea64218aba23
徐聪 <753428092@qq.com>
2021-12-16 14:30:15 +0800

    设计模式

--- deploy.sh
+++ deploy.sh
@@ -14,15 +14,15 @@ cd docs/.vuepress/dist
 # echo 'www.yourwebsite.com' > CNAME
 git init
 git add .
-git commit -m 'vue源码'
+git commit -m '设计模式'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f https://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@github.com/shamworld/shamworld.github.io.git master
+git push -f https://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@github.com/shamworld/shamworld.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
 
 cd -
 git add .
-git commit -m 'vue源码'
+git commit -m '设计模式'
 git push
